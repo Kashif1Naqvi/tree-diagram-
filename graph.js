1bc56ac0d84b043581dcd93c05b8f9e5d2940acf
Kashif1Naqvi <naqvishah582@gmail.com>
2019-11-15 11:31:33 +0500

    fix stratify sequence problem

--- graph.js
+++ graph.js
@@ -1,16 +1,19 @@
-let dim =   { height:500 , width:1100 };
+let  dim =   { height:500 , width:1100 };
 let svg  =  d3.select(".canvas").append("svg").attr("height",dim.height + 100).attr("width",dim.width + 100);
 let graph = svg.append("g").attr("transform","translate(50,50)");
+let data = []
 const stratify = d3.stratify()
-                   .id( d => d.name )
-                   .parentId( d => d.parent);
+.id(d => d[name])
+.parentId(d => d[parent])     
 const tree = d3.tree().size([dim.width , dim.height])
 const update = (data) => {
-    const rootNode = stratify(data)
-    const treeData = tree()
+ 
+    
+    const rootNode = stratify([data])
+    const treeData = tree(rootNode)
     const nodes = graph.selectAll(".node").data(treeData.descendants())
+    
 }
-let data = []
 db.collection("employees").onSnapshot(res=>{
   res.docChanges().forEach(change=>{
     let doc = {...change.doc.data(), id:change.doc.id}
